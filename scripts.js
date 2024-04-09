8e6aca54c084fecbd0b62a74d0dfc8ee36272bc3
dorianpreston <166086065+dorianpreston@users.noreply.github.com>
2024-04-08 23:47:36 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,26 @@ header {
   display: none !important;
 }
 
+const ratingStars = [...document.getElementsByClassName("rating__star")];
 
+function executeRating(stars) {
+  const starClassActive = "rating__star fas fa-star";
+  const starClassInactive = "rating__star far fa-star";
+  const starsLength = stars.length;
+  let i;
+  stars.map((star) => {
+    star.onclick = () => {
+      i = stars.indexOf(star);
+
+      if (star.className===starClassInactive) {
+        for (i; i >= 0; --i) stars[i].className = starClassActive;
+      } else {
+        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+      }
+    };
+  });
+}
+executeRating(ratingStars);
 }
 /** Dorians Notes!! Things to do 4/7/2024: Add search bar, add filter center 
 */
