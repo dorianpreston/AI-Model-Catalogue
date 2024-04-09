de878879ad7a9270b0b775090cd53f7cf8078499
dorianpreston <166086065+dorianpreston@users.noreply.github.com>
2024-04-09 05:30:10 -0700

    Update search.js

--- search.js
+++ search.js
@@ -5,13 +5,13 @@ function filterSelection(c) {
   if (c == "all") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
+    w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
 // Show filtered elements
-function AddClass(element, name) {
+function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
